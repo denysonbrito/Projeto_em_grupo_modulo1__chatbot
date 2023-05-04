8aeed9e1e0b4be31bbbdb17eb3b0175820867887
Fabiana Alves <fabileite653@gmail.com>
2023-05-04 19:40:24 -0300

    Arquivo das variáveis dos horários por modalidades
    Horário por modalidade

---
+++ arquivos do programa/horariodasmodalidades .py
@@ -0,0 +1,47 @@
+# criação de váriaveis para as telas dos horários das modadelidades
+horarioPorModalidade="""
+
+{0}ACADEMIA {1}VAMO AI
+
+HORÁRIOS POR MODALIDADE
+
+{0}1{1} - {0}MUSCULAÇÃO
+{0}2{1} - {0}RITMOS
+{0}3{1} - {0}LUTAS
+{0}0{1} - {0}VOLTAR
+
+"""
+horariomusculacao="""
+
+{0}ACADEMIA {1}VAMO AI
+
+        {0}HORÁRIO DA MUSCULAÇÃO{1} - 5h30 ÁS 23H59, TODOS OS DIAS!
+
+{0}0{1} -{0} voltar
+{0}1{1} -{0} sair
+
+"""
+
+horarioritmos="""
+
+{0}ACADEMIA {1}VAMO AI
+
+        {0}HORÁRIO RITMOS{1} -{0} SEGUNDA A SEXTA ÁS {1}20H15{0}
+
+{0}0{1} - {0}voltar
+{0}1{1} - {0}sair
+
+"""
+       
+horariolutas="""
+
+{0}ACADEMIA {1}VAMO AI
+
+        {0}HORÁRIO LUTAS{1} -{0} SEGUNDA A SEXTA ÁS {1}18H15{0}
+
+{0}0{1} -voltar 
+{0}1{1} - sair
+
+"""
+
+modalidadesPorHorario = [horarioPorModalidade, horariomusculacao,horarioritmos,horariolutas]
