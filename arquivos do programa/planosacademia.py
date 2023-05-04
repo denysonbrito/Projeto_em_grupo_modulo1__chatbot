da0c5db208aa40c5b9b6452571f754ea6e169982
LucsTx <lucstxx@gmail.com>
2023-05-04 19:46:01 -0300

    Planos academia
    Arquivo que contém os planos da academia

---
+++ arquivos do programa/planosacademia.py
@@ -0,0 +1,57 @@
+# criação de váriaveis para as telas dos planos
+
+planos="""
+
+{0}ACADEMIA{1} VAMO AI
+
+PLANOS
+
+{0}1{1}-{0} BRONZE
+{0}2{1}-{0} PRATA 
+{0}3{1}-{0} OURO 
+{0}0{1}-{0} VOLTAR
+
+"""
+
+planoBronze="""
+
+{0}ACADEMIA{1} VAMO AI
+
+BRONZE COMTEMPLA A MODADELIDADE MUSCULAÇÃO{0} 
+VALOR: R$100,00 POR MÊS
+
+{0}0{1} - {0}voltar
+{0}1{1} - {0}sair
+
+"""
+
+planoPrata="""
+
+{0}ACADEMIA{1} VAMO AI
+
+PRATA COMTEMPLA AS MODADELIDADES MUSCULAÇÃO + RITMOS{0}
+VALOR: R$110,00 POR MÊS
+
+{0}0{1} - {0}voltar
+{0}1{1} - {0}sair
+
+"""
+
+planoOuro="""
+
+{0}ACADEMIA{1} VAMO AI
+
+PLANO OURO COMTEMPLA AS MODADELIDADES MUSCULAÇÃO + RITMOS + LUTAS{0}
+VALOR: R$120,00 POR MÊS
+
+{0}0{1} - {0}voltar
+{0}1{1} - {0}sair
+
+"""
+
+tiposDeplanos = [planos,planoBronze,planoPrata,planoOuro]
+
+"""
+Aqui encadiamos as variáveis
+
+"""
