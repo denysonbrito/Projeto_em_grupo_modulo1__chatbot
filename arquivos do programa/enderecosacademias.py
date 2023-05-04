5a69f54d2aadbb12319fe3840419525d8ab8f062
Denyson de Brito <128834623+denysonbrito@users.noreply.github.com>
2023-05-04 19:21:19 -0300

    Arquivo endereço das academias
    O arquivo contém as variáveis dos endereços da academia e a lista para localizar a posição das academias

---
+++ arquivos do programa/enderecosacademias.py
@@ -0,0 +1,46 @@
+# criação de váriaveis para as telas dos endereços das unidades
+
+enderecoUnidades="""
+
+{0}ACADEMIA{1} VAMO AI
+
+{0}1{1} - {0}REGIÃO SUL
+{0}2{1} - {0}REGIÃO NORTE
+{0}3{1} - {0}REGIÃO LESTE
+{0}0{1} - {0}VOLTAR
+
+"""
+
+unidadeSul=""" 
+
+{0}ACADEMIA{1} VAMO AI
+
+RUA DOS COCAIS, 200 - BAIRRO NOVO
+
+{0}0{1} -{0} VOLTAR
+{0}1{1} -{0}  SAIR
+
+"""
+
+unidadeNorte="""
+
+{0}ACADEMIA{1} VAMO AI
+
+RUA DA PONTE, 300 - BAIRRO VILA VELHA
+
+
+{0}0{1} -{0} VOLTAR
+{0}1{1} -{0} SAIR
+
+"""
+unidadeLeste="""
+
+{0}ACADEMIA{1} VAMO AI
+
+RUA DAS OSTRAS 400 - BAIRRO CENTRO
+
+{0}0{1} - {0}VOLTAR
+{0}1{1} - {0}sair
+
+"""
+localizacaoacademias = [enderecoUnidades,unidadeSul,unidadeNorte,unidadeLeste]
